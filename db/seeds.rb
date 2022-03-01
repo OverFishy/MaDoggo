0561fd9bc40fe9134f936c9442290de7f2fdd917
ofek shtrit <ofek1997shtrit@gmail.com>
2022-03-01 18:52:12 +0200

    Deborah's commits

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,89 +1,82 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-puts 'Cleaning DB'
-User.delete_all
+# ---- Cleaning DB ----
+puts 'cleaning DB'
 Friendship.delete_all
+Park.delete_all
+Dog.delete_all
+User.delete_all
 puts 'DB cleaned'
 
-# user1 = User.create!(
-#   password: '1234567',
-#   email: 'o@o.com'
-# )
+# ---- creating users & friendship ----
+puts 'creating 2 users'
+user1 = User.create!(
+  password: '1234567',
+  email: 'o@o.com'
+)
 
-# user2 = User.create!(
-#   password: '1234567',
-#   email: 'l@l.com'
-# )
+user2 = User.create!(
+  password: '1234567',
+  email: 'l@l.com'
+)
+puts '2 users created'
 
-# Friendship.create!(
-#   user1: user1,
-#   user2: user2,
-#   status: 's'
-# )
+puts 'creating friendship'
+Friendship.create!(
+  user1: user1,
+  user2: user2,
+  status: 's'
+)
+puts 'friendship created'
 
-
-puts 'dogpark'
-Park.delete_all
-
-dogpark1 = Park.create!(
+# ---- creating 10 parks ----
+puts 'creating parks'
+Park.create!(
   name: 'Park Ha Yarkon',
-  location: 'Tel Aviv'
+  location: 'Hayarkon park Tel Aviv'
 )
 
-dogpark2 = Park.create!(
+Park.create!(
   name: 'Tverya Dog Park',
-  location: 'Tel Aviv'
+  location: 'rotchild Tel Aviv'
 )
 
-dogpark3 = Park.create!(
+Park.create!(
   name: 'Park Ha Yarkon Dog Park, Tel Aviv-Yafo',
-  location: 'Tel Aviv'
+  location: 'ben yehuda Tel Aviv'
 )
 
-dogpark4 = Park.create!(
+Park.create!(
   name: 'Park Ha Yarkon Dog Park, Tel Aviv-Yafo',
-  location: 'Tel Aviv'
+  location: 'frisheman Tel Aviv'
 )
 
-dogpark5 = Park.create!(
+Park.create!(
   name: 'Meir Garden Dog Park',
-  location: 'Tel Aviv'
+  location: 'givatayim israel'
 )
 
-dogpark6 = Park.create!(
+Park.create!(
   name: 'Kikar Hamedina Dog Park',
   location: 'Tel Aviv'
 )
 
-dogpark7 = Park.create!(
+Park.create!(
   name: 'Sheinkin Dog Park',
   location: 'Tel Aviv'
 )
 
-dogpark8 = Park.create!(
+Park.create!(
   name: 'Dubnov Dog park',
   location: 'Tel Aviv'
 )
 
-dogpark9 = Park.create!(
+Park.create!(
   name: 'Manshiya Dog Park',
   location: 'Tel Aviv'
 )
-dogpark10 = Park.create!(
+
+Park.create!(
   name: 'Dogs Park - Midron',
   location: 'Tel Aviv'
 )
 
-
-# t.string "name"
-# t.string "location"
-# t.float "latitude"
-# t.float "longitude"
-# t.datetime "created_at", null: false
-# t.datetime "updated_at", null: false
+puts 'Seed migrated!'
