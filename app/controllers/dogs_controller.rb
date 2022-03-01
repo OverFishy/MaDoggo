f8f598c6dc5f2ae824503910892668a498fac5ea
ofek shtrit <ofek1997shtrit@gmail.com>
2022-03-01 17:08:22 +0200

    add a dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,39 @@
 class DogsController < ApplicationController
+  def show
+    @dogs = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dogs = Dog.new(dog_params)
+    @dogs.user = current_user
+    if @dogs.save
+      redirect_to root_path, notice: 'New dog was created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_dog
+    @dogs = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :gender, :size, :traits, description: [])
+  end
 end
+
+# ARCHIVE
+
+   # def index
+   #   if params[:query].present?
+   #     @ = Dog.global_search(params[:query])
+   #   else
+   #     @dogs = Dog.all
+  #   end
+# end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,7 @@
 class Dog < ApplicationRecord
   belongs_to :user
+  has_many :parks
+  has_many_attached :photos
+  has_one_attached :main_photo
+  validates :name, :breed, :size, :gender, presence: true
 end
