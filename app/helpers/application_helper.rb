2225c87f5190d21c74e5030c1fd9e7b8e32a100a
ofek shtrit <ofek1997shtrit@gmail.com>
2022-03-01 18:04:36 +0200

    map bug fixed, friends name shows on dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def name_of_friend(friendship)
+    friendship.user1 == current_user ? friendship.user2.email : friendship.user1.email
+  end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,20 +2,21 @@ class Friendship < ApplicationRecord
   belongs_to :user1, class_name: 'User'
   belongs_to :user2, class_name: 'User'
   has_many :messages
-
-
-  def name_of_friends
-    friends = []
-    filtered_friends = []
-    current_user.all_my_friendships.each do |friendship|
-      user1 = friendship.user1
-      friends << user1
-      friendship.user2
-      friends << user2
-    end
-    friends.each do |friend|
-      filtered_friends << freind if friend != current_user
-    end
-    filtered_friends
-  end
 end
+
+# ARCHIVE
+
+  # def name_of_friends
+  #   friends = []
+  #   filtered_friends = []
+  #   current_user.all_my_friendships.each do |friendship|
+  #     user1 = friendship.user1
+  #     friends << user1
+  #     friendship.user2
+  #     friends << user2
+  #   end
+  #   friends.each do |friend|
+  #     filtered_friends << freind if friend != current_user
+  #   end
+  #   filtered_friends
+  # end
