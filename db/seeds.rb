d13755ae59b25042e485621150e8dcada212280f
deborah saverys <deborahsaverys1822@gmail.com>
2022-03-02 18:21:03 +0200

    savemappopup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 require 'faker'
+require "uri"
 
 # ---- Cleaning DB ----
 
 puts 'cleaning DB'
 Friendship.delete_all
+ParkVisit.delete_all
 Park.delete_all
 Dog.delete_all
 User.delete_all
@@ -54,55 +56,78 @@ puts '10 dogs created'
 
 puts 'creating parks'
 
-Park.create!(
+park1 = Park.create!(
   name: 'Ha-Yarkon dog park, Tel-Aviv',
-  location: 'Hayarkon park Tel Aviv'
+  location: 'Hayarkon park Tel Aviv',
 )
 
-Park.create!(
+file1 = URI.open('https://humaneheroes.org/wp-content/uploads/2019/09/txhh_best-practices-when-taking-dog-park.png')
+
+park1.photo.attach(io: file1, filename: 'park.png', content_type: 'image/png')
+
+park2 = Park.create!(
   name: 'kiryat sefer dog park, Tel-Aviv',
   location: 'kiryat sefer garden Tel Aviv'
 )
+file2 = URI.open('https://static.timesofisrael.com/www/uploads/2016/08/F150618MA013-640x400.jpg')
+park2.photo.attach(io: file2, filename: 'park.png', content_type: 'image/png')
 
-park = Park.create!(
+park3 = Park.create!(
   name: 'The independence dog park, Tel-Aviv',
   location: 'the independence park Tel Aviv'
 )
+file3 = URI.open('https://photos.bringfido.com/attractions/6/7/0/9076/9076_28834.png?size=slide&density=2x')
+park3.photo.attach(io: file3, filename: 'park.png', content_type: 'image/png')
 
-Park.create!(
+park4 = Park.create!(
   name: 'Frisheman dog park, Tel-Aviv',
   location: 'frisheman Tel Aviv'
 )
+file4 = URI.open('https://photos.bringfido.com/attractions/6/7/0/9076/9076_28834.png?size=slide&density=2x')
+park4.photo.attach(io: file4, filename: 'park.png', content_type: 'image/png')
 
-Park.create!(
+park5 = Park.create!(
   name: 'Arison Dog Park, Tel-Aviv',
   location: 'arison tel aviv'
 )
+file5 = URI.open('https://photos.bringfido.com/attractions/6/7/0/9076/9076_28834.png?size=slide&density=2x')
+park5.photo.attach(io: file5, filename: 'park.png', content_type: 'image/png')
 
-Park.create!(
+park6 = Park.create!(
   name: 'Gan meir Dog Park, Tel-Aviv',
   location: 'gan meir Tel Aviv'
 )
+file6 = URI.open('https://photos.bringfido.com/attractions/6/7/0/9076/9076_28834.png?size=slide&density=2x')
+park6.photo.attach(io: file6, filename: 'park.png', content_type: 'image/png')
 
-Park.create!(
+park7 = Park.create!(
   name: 'Dvora baron Dog Park, Tel-Aviv',
   location: 'dvora baron garden Tel Aviv'
 )
+file7 = URI.open('https://photos.bringfido.com/attractions/6/7/0/9076/9076_28834.png?size=slide&density=2x')
+park7.photo.attach(io: file7, filename: 'park.png', content_type: 'image/png')
 
-Park.create!(
+park8 = Park.create!(
   name: 'Sarona Dog park, Tel-Aviv',
   location: 'sarona park Tel Aviv'
 )
+file8 = URI.open('https://photos.bringfido.com/attractions/6/7/0/9076/9076_28834.png?size=slide&density=2x')
+park8.photo.attach(io: file8, filename: 'park.png', content_type: 'image/png')
 
-Park.create!(
+park9 = Park.create!(
   name: 'Dubnov Dog Park, Tel-Aviv',
   location: 'dubnov Tel Aviv'
 )
+file9 = URI.open('https://photos.bringfido.com/attractions/6/7/0/9076/9076_28834.png?size=slide&density=2x')
+park9.photo.attach(io: file9, filename: 'park.png', content_type: 'image/png')
 
-Park.create!(
+park10 = Park.create!(
   name: 'Zeev garden dog park, Tel-Aviv',
   location: 'zeev garden Tel Aviv'
 )
+file10 = URI.open('https://photos.bringfido.com/attractions/6/7/0/9076/9076_28834.png?size=slide&density=2x')
+park10.photo.attach(io: file10, filename: 'park.png', content_type: 'image/png')
+
 puts 'parks created'
 
 # ---- creating parks visits ----
@@ -110,7 +135,7 @@ puts 'parks created'
 puts 'creating park visit'
 
 ParkVisit.create!(
-  park: park,
+  park: park1,
   user: user2
 )
 
