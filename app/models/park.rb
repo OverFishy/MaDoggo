b3c69cc03f42512d360ece16fc8706021757baf4
deborah saverys <deborahsaverys1822@gmail.com>
2022-03-03 12:51:53 +0200

    number on the popup

--- app/models/park.rb
+++ app/models/park.rb
@@ -3,6 +3,8 @@ class Park < ApplicationRecord
   has_many :park_messages
   has_many :park_reviews
   has_many :park_visits
+  has_many :users, through: :park_visits
+  has_many :dogs, through: :users
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
