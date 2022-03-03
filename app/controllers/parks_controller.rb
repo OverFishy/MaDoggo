5406a334e7eb19021acf8e77fed0a44e6b002f28
luk ross <lukerosssa@gmail.com>
2022-03-03 14:14:40 +0200

    updated park show page

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -14,18 +14,18 @@ class ParksController < ApplicationController
   end
 
 
-def show
-  @park = Park.find(params[:id])
-end
+  def show
+    @park = Park.find(params[:id])
+  end
 
 
-private
+  private
 
-def find_park
-  @parks = Park.find(params[:id])
-end
+  def find_park
+    @parks = Park.find(params[:id])
+  end
 
-def park_params
-  params.require(:parks).permit(:name, :location, :latitude, :longitude)
- end
+  def park_params
+    params.require(:parks).permit(:name, :location, :latitude, :longitude)
+  end
 end
