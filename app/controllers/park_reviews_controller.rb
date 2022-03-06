0a77d73c60ef7958424e54e26a08206535a6304f
luk ross <lukerosssa@gmail.com>
2022-03-06 11:40:51 +0200

    Park reeview skeleton

--- app/controllers/park_reviews_controller.rb
+++ app/controllers/park_reviews_controller.rb
@@ -1,2 +1,27 @@
 class ParkReviewsController < ApplicationController
+  def new
+    @park = Park.find(params[:park_id])
+    @review = ParkReview.new
+  end
+
+  def create
+    @review = ParkReview.new(review_params)
+    #@park = Park.find(params[:park_id])
+    @park.reviews = @review
+
+    if @review.save
+      redirect_to park_path(@park)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
+
+
+#.permit(:text, :stars, :user_id, :park_id)
