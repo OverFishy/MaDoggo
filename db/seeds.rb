6d453caba7fd0ad45e3b678f1204b6fc8ad80e76
ofek shtrit <ofek1997shtrit@gmail.com>
2022-03-07 14:27:11 +0200

    small seed touchups

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,55 +17,51 @@ puts 'DB cleaned'
 # # ---- creating users & friendship ----
 
 puts 'creating 4 users'
-
 ofek = User.create!(
   password: '1234567',
-  email: 'ofek@gmail.com',
-  name: 'Ofek'
+  email: "ofek@gmail.com",
+  name: "ofek"
 )
-
-luke = User.create!(
-  password: '1234567',
-  email: 'luke@gmail.com',
-  name: 'Luke'
-)
-
 daniel = User.create!(
   password: '1234567',
-  email: 'daniel@gmail.com',
-  name: 'Daniel'
+  email: "daniel@gmail.com",
+  name: "daniel"
 )
-
 deborah = User.create!(
   password: '1234567',
-  email: 'deborah@gmail.com',
-  name: 'Deborah'
+  email: "deborah@gmail.com",
+  name: "deborah"
+)
+luke = User.create!(
+  password: '1234567',
+  email: "luke@gmail.com",
+  name: "luke"
 )
 puts '4 users created'
 
-# puts 'creating friendships'
+puts 'creating friendships'
 
-# Friendship.create!(
-#   user1: ofek,
-#   user2: luke,
-#   status: 's'
-# )
+Friendship.create!(
+  user1: ofek,
+  user2: luke,
+  status: 's'
+)
 
-# Friendship.create!(
-#   user1: ofek,
-#   user2: daniel,
-#   status: 's'
-# )
+Friendship.create!(
+  user1: ofek,
+  user2: daniel,
+  status: 's'
+)
 
-# Friendship.create!(
-#   user1: ofek,
-#   user2: deborah,
-#   status: 's'
-# )
+Friendship.create!(
+  user1: ofek,
+  user2: deborah,
+  status: 's'
+)
 
-# puts 'friendships created'
+puts 'friendships created'
 
-# ---- Creating dog traits ----
+#---- Creating dog traits ----
 
 puts 'Creating traits'
 trait_names = %w[senior fluffy playful pupper introvert zoomies friendly
@@ -88,7 +84,7 @@ dog_data = [
     breed: :dalmatian,
     age: 4,
     size: :big,
-    user: [ofek, deborah, luke, daniel].sample,
+    user: ofek,
     traits: %w[calm timid intelligent],
     photo_url: 'https://cdn.britannica.com/60/12460-004-54537CCC.jpg'
   },
@@ -274,115 +270,115 @@ dog_data.each do |data|
   dog.main_photo.attach(io: file, filename: 'dog.png', content_type: 'image/png')
 end
 
-# # ---- creating 10 parks ----
+# ---- creating 10 parks ----
 
-# puts 'creating parks'
+puts 'creating parks'
 
-# hayarkon = Park.create!(
-#   name: 'Ha-Yarkon dog park, Tel-Aviv',
-#   location: 'Hayarkon park Tel Aviv'
-# )
+hayarkon = Park.create!(
+  name: 'Ha-Yarkon dog park, Tel-Aviv',
+  location: 'Hayarkon park Tel Aviv'
+)
 
-# file1 = URI.open('https://humaneheroes.org/wp-content/uploads/2019/09/txhh_best-practices-when-taking-dog-park.png')
+file1 = URI.open('https://humaneheroes.org/wp-content/uploads/2019/09/txhh_best-practices-when-taking-dog-park.png')
 
-# hayarkon.photo.attach(io: file1, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 1'
+hayarkon.photo.attach(io: file1, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 1'
 
-# park2 = Park.create!(
-#   name: 'kiryat sefer dog park, Tel-Aviv',
-#   location: 'kiryat sefer garden Tel Aviv'
-# )
-# file2 = URI.open('https://static.timesofisrael.com/www/uploads/2016/08/F150618MA013-640x400.jpg')
-# park2.photo.attach(io: file2, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 2'
+park2 = Park.create!(
+  name: 'kiryat sefer dog park, Tel-Aviv',
+  location: 'kiryat sefer garden Tel Aviv'
+)
+file2 = URI.open('https://static.timesofisrael.com/www/uploads/2016/08/F150618MA013-640x400.jpg')
+park2.photo.attach(io: file2, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 2'
 
-# independence = Park.create!(
-#   name: 'The independence dog park, Tel-Aviv',
-#   location: 'the independence park Tel Aviv'
-# )
+independence = Park.create!(
+  name: 'The independence dog park, Tel-Aviv',
+  location: 'the independence park Tel Aviv'
+)
 
-# file3 = URI.open('https://media.istockphoto.com/photos/dogs-playing-at-public-park-picture-id1210828938?k=20&m=1210828938&s=612x612&w=0&h=A0iF7IGhJryX35xgP4jCgiG0vIvUsduUTxQmlL2boVc=')
-# independence.photo.attach(io: file3, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 3'
+file3 = URI.open('https://media.istockphoto.com/photos/dogs-playing-at-public-park-picture-id1210828938?k=20&m=1210828938&s=612x612&w=0&h=A0iF7IGhJryX35xgP4jCgiG0vIvUsduUTxQmlL2boVc=')
+independence.photo.attach(io: file3, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 3'
 
-# park4 = Park.create!(
-#   name: 'Frisheman dog park, Tel-Aviv',
-#   location: 'frisheman Tel Aviv'
-# )
+park4 = Park.create!(
+  name: 'Frisheman dog park, Tel-Aviv',
+  location: 'frisheman Tel Aviv'
+)
 
-# file4 = URI.open('https://media.istockphoto.com/photos/five-cute-dogs-lined-up-in-a-row-picture-id468341718?k=20&m=468341718&s=612x612&w=0&h=Hm_KonfznJ_0NHvY7I-PpaLuaoik7CXRYdWn4dzH0BI=')
-# park4.photo.attach(io: file4, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 4'
+file4 = URI.open('https://media.istockphoto.com/photos/five-cute-dogs-lined-up-in-a-row-picture-id468341718?k=20&m=468341718&s=612x612&w=0&h=Hm_KonfznJ_0NHvY7I-PpaLuaoik7CXRYdWn4dzH0BI=')
+park4.photo.attach(io: file4, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 4'
 
-# park5 = Park.create!(
-#   name: 'Arison Dog Park, Tel-Aviv',
-#   location: 'arison tel aviv'
-# )
-# file5 = URI.open('https://media.istockphoto.com/photos/dogs-running-with-black-dog-off-ground-picture-id1143172074?k=20&m=1143172074&s=612x612&w=0&h=Eey7FCx0CavXyGlczHRX3MWHcSSEIh2hnfYf74S1XUA=')
-# park5.photo.attach(io: file5, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 5'
+park5 = Park.create!(
+  name: 'Arison Dog Park, Tel-Aviv',
+  location: 'arison tel aviv'
+)
+file5 = URI.open('https://media.istockphoto.com/photos/dogs-running-with-black-dog-off-ground-picture-id1143172074?k=20&m=1143172074&s=612x612&w=0&h=Eey7FCx0CavXyGlczHRX3MWHcSSEIh2hnfYf74S1XUA=')
+park5.photo.attach(io: file5, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 5'
 
-# park6 = Park.create!(
-#   name: 'Gan meir Dog Park, Tel-Aviv',
-#   location: 'gan meir Tel Aviv'
-# )
-# file6 = URI.open('https://media.istockphoto.com/photos/golden-retriever-having-a-drink-picture-id946655044?k=20&m=946655044&s=612x612&w=0&h=tXumZN9KGJZkc6ObHx0dfVOs_wP-T61axybQlH7UjL0=')
-# park6.photo.attach(io: file6, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 6'
+park6 = Park.create!(
+  name: 'Gan meir Dog Park, Tel-Aviv',
+  location: 'gan meir Tel Aviv'
+)
+file6 = URI.open('https://media.istockphoto.com/photos/golden-retriever-having-a-drink-picture-id946655044?k=20&m=946655044&s=612x612&w=0&h=tXumZN9KGJZkc6ObHx0dfVOs_wP-T61axybQlH7UjL0=')
+park6.photo.attach(io: file6, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 6'
 
-# park7 = Park.create!(
-#   name: 'Dvora baron Dog Park, Tel-Aviv',
-#   location: 'dvora baron garden Tel Aviv'
-# )
-# file7 = URI.open('https://media.istockphoto.com/photos/dogs-joyfully-play-and-run-in-dog-park-picture-id1326514093?k=20&m=1326514093&s=612x612&w=0&h=N0o_3Ui6zQdyNiFWqTSoCbvn-brbjx4x2zCUFHRcQ_s=')
-# park7.photo.attach(io: file7, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 7'
+park7 = Park.create!(
+  name: 'Dvora baron Dog Park, Tel-Aviv',
+  location: 'dvora baron garden Tel Aviv'
+)
+file7 = URI.open('https://media.istockphoto.com/photos/dogs-joyfully-play-and-run-in-dog-park-picture-id1326514093?k=20&m=1326514093&s=612x612&w=0&h=N0o_3Ui6zQdyNiFWqTSoCbvn-brbjx4x2zCUFHRcQ_s=')
+park7.photo.attach(io: file7, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 7'
 
-# park8 = Park.create!(
-#   name: 'Sarona Dog park, Tel-Aviv',
-#   location: 'sarona park Tel Aviv'
-# )
-# file8 = URI.open('https://media.istockphoto.com/photos/happy-dog-playing-at-the-park-picture-id1223104509?k=20&m=1223104509&s=612x612&w=0&h=WrNl1LIWbEpreTemWN4Tu8c1fivbtkpCjSnttFSgJGg=')
-# park8.photo.attach(io: file8, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 8'
+park8 = Park.create!(
+  name: 'Sarona Dog park, Tel-Aviv',
+  location: 'sarona park Tel Aviv'
+)
+file8 = URI.open('https://media.istockphoto.com/photos/happy-dog-playing-at-the-park-picture-id1223104509?k=20&m=1223104509&s=612x612&w=0&h=WrNl1LIWbEpreTemWN4Tu8c1fivbtkpCjSnttFSgJGg=')
+park8.photo.attach(io: file8, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 8'
 
-# park9 = Park.create!(
-#   name: 'Dubnov Dog Park, Tel-Aviv',
-#   location: 'dubnov Tel Aviv'
-# )
-# file9 = URI.open('https://media.istockphoto.com/photos/dogs-playing-with-dog-runs-picture-id1293404176?k=20&m=1293404176&s=612x612&w=0&h=0AjeIzUc0cGSWmcwSXgGRbO7wqn72rSQRPrnld9fB3M=')
-# park9.photo.attach(io: file9, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 9'
+park9 = Park.create!(
+  name: 'Dubnov Dog Park, Tel-Aviv',
+  location: 'dubnov Tel Aviv'
+)
+file9 = URI.open('https://media.istockphoto.com/photos/dogs-playing-with-dog-runs-picture-id1293404176?k=20&m=1293404176&s=612x612&w=0&h=0AjeIzUc0cGSWmcwSXgGRbO7wqn72rSQRPrnld9fB3M=')
+park9.photo.attach(io: file9, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 9'
 
-# park10 = Park.create!(
-#   name: 'Zeev garden dog park, Tel-Aviv',
-#   location: 'zeev garden Tel Aviv'
-# )
-# file10 = URI.open('https://d34c09ztlk5mrb.cloudfront.net/cunningham-recreation/d2sacxxyngi6cb_cloudfront_net/Dog-Park-Image_183d17ce614a305ef7e14498bef98c4b.jpg')
-# park10.photo.attach(io: file10, filename: 'park.png', content_type: 'image/png')
-# puts 'att photo park 10'
+park10 = Park.create!(
+  name: 'Zeev garden dog park, Tel-Aviv',
+  location: 'zeev garden Tel Aviv'
+)
+file10 = URI.open('https://d34c09ztlk5mrb.cloudfront.net/cunningham-recreation/d2sacxxyngi6cb_cloudfront_net/Dog-Park-Image_183d17ce614a305ef7e14498bef98c4b.jpg')
+park10.photo.attach(io: file10, filename: 'park.png', content_type: 'image/png')
+puts 'att photo park 10'
 
-# puts 'parks created'
+puts 'parks created'
 
-# # ---- creating parks visits ----
+# ---- creating parks visits ----
 
-# puts 'creating 3 park visit'
+puts 'creating 3 park visit'
 
-# ParkVisit.create!(
-#   park: hayarkon,
-#   user: luke
-# )
+ParkVisit.create!(
+  park: hayarkon,
+  user: luke
+)
 
-# ParkVisit.create!(
-#   park: independence,
-#   user: daniel
-# )
+ParkVisit.create!(
+  park: independence,
+  user: daniel
+)
 
-# ParkVisit.create!(
-#   park: independence,
-#   user: deborah
-# )
+ParkVisit.create!(
+  park: independence,
+  user: deborah
+)
 
-# puts '3 park visit created'
+puts '3 park visit created'
 
 puts 'Seed migrated!'
