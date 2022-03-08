fa4c347bff7bbb11713995b1bd7f84ce74208c78
ofek shtrit <ofek1997shtrit@gmail.com>
2022-03-09 00:05:52 +0200

    added gender btns

--- app/javascript/controllers/add_dog_controller.js
+++ app/javascript/controllers/add_dog_controller.js
@@ -5,14 +5,18 @@ export default class extends Controller {
   static values = {
     id: String
   }
-  static targets = ['trait', 'form', 'traitInput', 'traitValues']
+  static targets = [
+    'trait', 'form', 'traitInput', 'traitValues',
+    'gender',
+  ]
 
   connect() {
-    console.log('Hello');
+    console.log('Hello,', this.element);
+    this.femaleButton = document.getEleme
   }
 
   traitElementClicked(evt) {
-    console.log(evt, evt.target, this.idValue );
+    console.log(evt, evt.target, this.idValue);
     const traitName = evt.target.innerText;
     console.log(traitName, this.formTarget, this.traitInputTarget);
     evt.target.classList.toggle('trait-picked');
@@ -27,10 +31,38 @@ export default class extends Controller {
   }
 
   genderElementClicked(evt) {
-    console.log(evt, evt.target);
+    // console.log(evt, evt.target);
+    // expect to see 2 gender button elements
+    console.log(this.genderTargets);
     // add value to each gender icon ( data-value from stimulous )
-    const gender = evt.target.getAttribute('data-gender');
-    console.log(gender)
+    const genders = evt.target.getAttribute('data-gender');
+
+    // remove both actives
+    this.genderTargets[1].classList.remove('gender-active');
+    this.genderTargets[0].classList.remove('gender-active');
+
+    // Array.from(genders).forEach(el => {
+    //   console.log({el});
+    //   el.classList.remove('gender-active');
+    // })
+
+    evt.target.classList.toggle('gender-active');
+    // add active to clicked
+
+    // none active, press one
+    // turn on
+
+    // one active, press it
+    // turn off
+
+    // one active, press the the other
+    // remove from other
+    // add to you
+
+    // evt.target.classList.toggle('');
+    //
+
+    // console.log(gender)
     // grab value from clicked icon
     // assign clicked value to gender input
     // this.genderInputTarget.value = gender;
